183b20be6b8ef09792d8b747673bff4647388d16
Brian <thinhle1199@gmail.com>
2025-01-09 20:46:46 +0700

    config initial routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from './components/Login.tsx';
+import { setupStore } from './store/store.ts';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const store = setupStore();
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path = '/' element = {<App />}>
+    <Route path = 'login' element = {<Login />} />
+  </Route >
+));
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <Provider store = {store}>
+    <RouterProvider router = {router} />
+  </Provider>
 )
